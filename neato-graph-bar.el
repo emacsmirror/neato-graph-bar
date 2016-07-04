52cdbd1ff9e50d9ac9f5b20004bccf3564aeeb4d
Robert Cochran <robert-git@cochranmail.com>
2016-07-03 21:53:58 -0700

    Remove whitespace in lambda in get-memory-info

--- neato-graph-bar.el
+++ neato-graph-bar.el
@@ -258,9 +258,7 @@ filtered down to entries listed in `neato-graph-bar/memory-fields-to-keep'."
     (cl-delete-if-not (lambda (x) (member x neato-graph-bar/memory-fields-to-keep))
 		      mem-info-list
 		      :key #'car)
-    (mapc (lambda (x)
-	    (rplacd x
-		    (string-to-number (car (split-string (cadr x))))))
+    (mapc (lambda (x) (rplacd x (string-to-number (car (split-string (cadr x))))))
 	  mem-info-list)))
 
 (defun neato-graph-bar/get-memory-attribute (alist attribute)
