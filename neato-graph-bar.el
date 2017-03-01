ca7b704165e51e31bb9f51c9042f72aeab770652
Robert Cochran <robert-git@cochranmail.com>
2017-03-01 08:07:26 -0800

    Rename symbols "correctly"

--- neato-graph-bar.el
+++ neato-graph-bar.el
@@ -26,7 +26,7 @@
   "Neat-o graph bars for various things"
   :group 'applications)
 
-(defcustom neato-graph-bar/memory-info-file
+(defcustom neato-graph-bar-memory-info-file
   "/proc/meminfo"
   "File to grab the memory usage info from.
 
@@ -34,7 +34,7 @@ Currently expecting Linux meminfo format."
   :type '(file :must-match t)
   :group 'neato-graph-bar)
 
-(defcustom neato-graph-bar/cpu-stat-file
+(defcustom neato-graph-bar-cpu-stat-file
   "/proc/stat"
   "File to grab the CPU usage statistics from.
 
@@ -42,74 +42,74 @@ Currently expecting Linux /proc/stat format."
   :type '(file :must-match t)
   :group 'neato-graph-bar)
 
-(defcustom neato-graph-bar/unified-cpu-graph
+(defcustom neato-graph-bar-unified-cpu-graph
   nil
   "When nil, draw separate graphs for each CPU.
 When non-nil, draw a single unified CPU graph."
   :type 'boolean)
 
-(defcustom neato-graph-bar/label-padding
+(defcustom neato-graph-bar-label-padding
   4
   "The number of character to pad graph labels to."
   :type 'wholenum
   :group 'neato-graph-bar)
 
-(defcustom neato-graph-bar/refresh-time
+(defcustom neato-graph-bar-refresh-time
   1
   "The number of seconds to wait before refreshing."
   :type 'number
   :group 'neato-graph-bar)
 
-(defface neato-graph-bar/memory-used
+(defface neato-graph-bar-memory-used
   '((t
      :foreground "green"
      :inherit default))
   "Face for the portion of the memory graph representing userspace memory"
   :group 'neato-graph-bar)
 
-(defface neato-graph-bar/memory-buffer
+(defface neato-graph-bar-memory-buffer
   '((t
      :foreground "blue"
      :inherit default))
   "Face for the portion of the memory graph representing buffers"
   :group 'neato-graph-bar)
 
-(defface neato-graph-bar/memory-cache
+(defface neato-graph-bar-memory-cache
   '((t
      :foreground "yellow"
      :inherit default))
   "Face for the portion of the memory graph representing caches"
   :group 'neato-graph-bar)
 
-(defface neato-graph-bar/cpu-user
+(defface neato-graph-bar-cpu-user
   '((t
      :foreground "green"
      :inherit default))
   "Face for the portion of the CPU graph representing user programs"
   :group 'neato-graph-bar)
 
-(defface neato-graph-bar/cpu-system
+(defface neato-graph-bar-cpu-system
   '((t
      :foreground "red"
      :inherit default))
   "Face for the portion of the CPU graph representing the kernel"
   :group 'neato-graph-bar)
 
-(defface neato-graph-bar/cpu-interrupt
+(defface neato-graph-bar-cpu-interrupt
   '((t
      :foreground "gray"
      :inherit default))
   "Face for the portion of the CPU graph representing interrupts"
   :group 'neato-graph-bar)
 
-(defface neato-graph-bar/cpu-vm
+(defface neato-graph-bar-cpu-vm
   '((t
      :foreground "purple"
      :inherit default))
   "Face for the portion of the CPU graph representing virtual machines"
   :group 'neato-graph-bar)
 
-(defvar-local neato-graph-bar/memory-fields-to-keep
+(defvar-local neato-graph-bar--memory-fields-to-keep
   '("MemTotal"
     "MemFree"
     "MemAvailable"
@@ -122,7 +122,7 @@ When non-nil, draw a single unified CPU graph."
 memory use low and not have a bunch of unused info in the
 alist.")
 
-(defvar-local neato-graph-bar/cpu-field-names
+(defvar-local neato-graph-bar--cpu-field-names
   '(user
     nice
     system
@@ -135,15 +135,15 @@ alist.")
     guest-nice)
   "Field names for CPU statistics")
 
-(defvar-local neato-graph-bar/cpu-stats-previous
+(defvar-local neato-graph-bar--cpu-stats-previous
   nil
   "Previous statistics of CPU usage information")
 
-(defvar-local neato-graph-bar/update-timer
+(defvar-local neato-graph-bar--update-timer
   nil
   "Update timer for graphs")
 
-(defvar-local neato-graph-bar/current-window
+(defvar-local neato-graph-bar--current-window
   nil
   "Current window")
 
@@ -156,45 +156,45 @@ alist.")
     (buffer-disable-undo buffer)
     (when (zerop (buffer-size))
       (neato-graph-bar-mode)
-      (neato-graph-bar/update))
+      (neato-graph-bar-update))
     (pop-to-buffer buffer)
-    (unless neato-graph-bar/update-timer
-      (setq neato-graph-bar/update-timer
+    (unless neato-graph-bar--update-timer
+      (setq neato-graph-bar--update-timer
 	    (run-at-time 0
-			 neato-graph-bar/refresh-time
-			 'neato-graph-bar/update)))))
+			 neato-graph-bar-refresh-time
+			 'neato-graph-bar-update)))))
 
 ;;;###autoload
 (define-derived-mode neato-graph-bar-mode
   special-mode
   "Neato Graph Bar"
   (set (make-local-variable 'revert-buffer-function)
-       'neato-graph-bar/update)
+       'neato-graph-bar-update)
   (setq cursor-type nil)
   (add-hook 'kill-buffer-hook
 	    (lambda ()
-	      (if (timerp neato-graph-bar/update-timer)
-		  (cancel-timer neato-graph-bar/update-timer)))))
+	      (if (timerp neato-graph-bar--update-timer)
+		  (cancel-timer neato-graph-bar--update-timer)))))
 
-(defun neato-graph-bar/update ()
+(defun neato-graph-bar-update ()
   "Update the graphs."
   (dolist (buffer (buffer-list))
     (with-current-buffer buffer
       (when (eq major-mode 'neato-graph-bar-mode)
-	(setq neato-graph-bar/current-window (get-buffer-window nil t))
-	(if neato-graph-bar/current-window
-	    (neato-graph-bar/redraw))))))
+	(setq neato-graph-bar-current-window (get-buffer-window nil t))
+	(if neato-graph-bar--current-window
+	    (neato-graph-bar--redraw))))))
 
-(defun neato-graph-bar/redraw ()
+(defun neato-graph-bar--redraw ()
   "Neato Graph Bar revert function."
-  (if (zerop (window-width neato-graph-bar/current-window))
-      (return-from neato-graph-bar/update))
+  (if (zerop (window-width neato-graph-bar--current-window))
+      (cl-return-from neato-graph-bar-update))
   (let ((buffer-read-only nil))
     (erase-buffer)
-    (neato-graph-bar/draw-all-graphs))
+    (neato-graph-bar--draw-all-graphs))
   (goto-char (point-min)))
 
-(defun neato-graph-bar/draw-graph (label portions &optional end-text)
+(defun neato-graph-bar--draw-graph (label portions &optional end-text)
   "Draw a bar graph.
 
 LABEL is the label in front of the graph.  PORTIONS is an alist
@@ -211,10 +211,10 @@ for PORTIONS.  END-TEXT is placed within the graph at the
 end.  Unspecified, it defaults to a percentage, but can be any
 arbitrary string (good for doing things such as providing a
 \"30MB/100MB\" type counter for storage graphs)."
-  (let* ((padded-label (concat (make-string (- neato-graph-bar/label-padding
+  (let* ((padded-label (concat (make-string (- neato-graph-bar-label-padding
                                                (length label)) ?\s)
                                label))
-         (bar-width (- (window-body-width neato-graph-bar/current-window)
+         (bar-width (- (window-body-width neato-graph-bar--current-window)
                        ;; Seems that Emacs will wrap the line if it extends
                        ;; all the way to the end of the window in a terminal...
                        ;; not what we want... so adjust for it.
@@ -241,7 +241,7 @@ arbitrary string (good for doing things such as providing a
       (insert end-text)
       (delete-char (length end-text)))))
 
-(defun neato-graph-bar/create-storage-status-text (used total)
+(defun neato-graph-bar--create-storage-status-text (used total)
   "Create an end-text suitable for storage bars (ie \"10M/200M\").
 
 USED and TOTAL should both be in kilobytes."
@@ -254,25 +254,25 @@ USED and TOTAL should both be in kilobytes."
 	    (/ (float total) (expt 1024 log-total))
 	    (elt suffix-table log-total))))
 
-(defun neato-graph-bar/get-memory-info ()
+(defun neato-graph-bar--get-memory-info ()
   "Retrieve the system memory information.
 
-This is obtained from `neato-graph-bar/memory-info-file', and
-filtered down to entries listed in `neato-graph-bar/memory-fields-to-keep'."
+This is obtained from `neato-graph-bar-memory-info-file', and
+filtered down to entries listed in `neato-graph-bar--memory-fields-to-keep'."
   (let ((mem-info-list
 	 (mapcar (lambda (x) (split-string x ":" t))
 		 (with-temp-buffer
-		   (insert-file-contents neato-graph-bar/memory-info-file)
+		   (insert-file-contents neato-graph-bar-memory-info-file)
 		   (split-string (buffer-string) "\n" t)))))
-    (cl-delete-if-not (lambda (x) (member x neato-graph-bar/memory-fields-to-keep))
+    (cl-delete-if-not (lambda (x) (member x neato-graph-bar--memory-fields-to-keep))
 		      mem-info-list
 		      :key #'car)
     (mapc (lambda (x) (rplacd x (string-to-number (car (split-string (cadr x))))))
 	  mem-info-list)))
 
-(defun neato-graph-bar/draw-memory-graph ()
+(defun neato-graph-bar--draw-memory-graph ()
   "Draw memory graph."
-  (let ((memory-info (neato-graph-bar/get-memory-info)))
+  (let ((memory-info (neato-graph-bar--get-memory-info)))
     (cl-flet ((get-attribute (x) (cdr (assoc x memory-info))))
       (let* ((memory-total (get-attribute "MemTotal"))
 	     (memory-free (get-attribute "MemFree"))
@@ -286,67 +286,67 @@ filtered down to entries listed in `neato-graph-bar/memory-fields-to-keep'."
 	     ;; from procps-ng uses the first form, so I do here as well.
 	     (memory-used (- memory-total memory-free memory-buffers memory-cached))
 	     (memory-graph-alist
-	      `((neato-graph-bar/memory-used . ,(/ (float memory-used)
+	      `((neato-graph-bar-memory-used . ,(/ (float memory-used)
 						   memory-total))
-		(neato-graph-bar/memory-buffer . ,(/ (float memory-buffers)
+		(neato-graph-bar-memory-buffer . ,(/ (float memory-buffers)
 						     memory-total))
-		(neato-graph-bar/memory-cache . ,(/ (float memory-cached)
+		(neato-graph-bar-memory-cache . ,(/ (float memory-cached)
 						    memory-total))))
-	     (memory-end-text (neato-graph-bar/create-storage-status-text
+	     (memory-end-text (neato-graph-bar--create-storage-status-text
 			       memory-used
 			       memory-total)))
-	(neato-graph-bar/draw-graph "Mem" memory-graph-alist memory-end-text)))))
+	(neato-graph-bar--draw-graph "Mem" memory-graph-alist memory-end-text)))))
 
-(defun neato-graph-bar/draw-swap-graph ()
+(defun neato-graph-bar--draw-swap-graph ()
   "Draw swap graph."
-  (let ((memory-info (neato-graph-bar/get-memory-info)))
+  (let ((memory-info (neato-graph-bar--get-memory-info)))
     (cl-flet ((get-attribute (x) (cdr (assoc x memory-info))))
       (let* ((swap-total (get-attribute "SwapTotal"))
 	     (swap-free (get-attribute "SwapFree"))
 	     (swap-cached (get-attribute "SwapCached"))
 	     (swap-used (- swap-total swap-free swap-cached))
 	     (swap-graph-alist
-	      `((neato-graph-bar/memory-used . ,(/ (float swap-used) swap-total))
-		(neato-graph-bar/memory-cache . ,(/ (float swap-cached)
+	      `((neato-graph-bar-memory-used . ,(/ (float swap-used) swap-total))
+		(neato-graph-bar-memory-cache . ,(/ (float swap-cached)
 						    swap-total))))
-	     (swap-end-text (neato-graph-bar/create-storage-status-text
+	     (swap-end-text (neato-graph-bar--create-storage-status-text
 			     swap-used
 			     swap-total)))
-	(neato-graph-bar/draw-graph "Swap" swap-graph-alist swap-end-text)))))
+	(neato-graph-bar--draw-graph "Swap" swap-graph-alist swap-end-text)))))
 
-(defun neato-graph-bar/get-cpu-stats ()
+(defun neato-graph-bar--get-cpu-stats ()
   "Get the difference in the system CPU statistics since last update.
 
-The old information snapshot is moved to `neato-graph-bar/cpu-stats-previous'.
+The old information snapshot is moved to `neato-graph-bar--cpu-stats-previous'.
 
-This is obtained from `neato-graph-bar/cpu-stat-file', and made
-into key-value pairs as defined by `neato-graph-bar/cpu-field-names'."
+This is obtained from `neato-graph-bar-cpu-stat-file', and made
+into key-value pairs as defined by `neato-graph-bar--cpu-field-names'."
   (let* ((cpu-stat-list-strings
 	  (mapcar (lambda (x) (split-string x " " t))
 		  (with-temp-buffer
-		    (insert-file-contents neato-graph-bar/cpu-stat-file)
+		    (insert-file-contents neato-graph-bar-cpu-stat-file)
 		    (cl-delete-if-not
 		     (lambda (x) (string= (substring x 0 3) "cpu"))
 		     (split-string (buffer-string) "\n" t)))))
 	 (cpu-stat-list
 	  (mapcar (lambda (x)
 		    (cons (car x)
-			  (cl-pairlis neato-graph-bar/cpu-field-names
+			  (cl-pairlis neato-graph-bar--cpu-field-names
 				      (mapcar #'string-to-number (cdr x)))))
 		  cpu-stat-list-strings))
 	 (cpu-diff (copy-tree cpu-stat-list)))
     ;; Account for empty first run
-    (unless neato-graph-bar/cpu-stats-previous
-      (setq neato-graph-bar/cpu-stats-previous cpu-stat-list))
+    (unless neato-graph-bar--cpu-stats-previous
+      (setq neato-graph-bar--cpu-stats-previous cpu-stat-list))
     (cl-map 'list (lambda (n o)
 		    (cl-map 'list (lambda (x y)
 				    (rplacd x (- (cdr x) (cdr y))))
 			    (cdr n) (cdr o)))
-	    cpu-diff neato-graph-bar/cpu-stats-previous)
-    (setq neato-graph-bar/cpu-stats-previous cpu-stat-list)
+	    cpu-diff neato-graph-bar--cpu-stats-previous)
+    (setq neato-graph-bar--cpu-stats-previous cpu-stat-list)
     cpu-diff))
 
-(defun neato-graph-bar/draw-cpu-graph-helper (cpu)
+(defun neato-graph-bar--draw-cpu-graph-helper (cpu)
   "Helper to draw the CPU graph(s).  Does the actual work."
   (cl-flet ((stat-total () (cl-reduce #'+ (cdr cpu) :key #'cdr))
 	    (get-attribute (x) (cdr (assoc x (cdr cpu)))))
@@ -361,32 +361,32 @@ into key-value pairs as defined by `neato-graph-bar/cpu-field-names'."
 		      (get-attribute 'guest)
 		      (get-attribute 'guest-nice)))
 	   (cpu-graph-alist
-	    `((neato-graph-bar/cpu-user . ,(/ (float cpu-user) cpu-total))
-	      (neato-graph-bar/cpu-system . ,(/ (float cpu-system) cpu-total))
-	      (neato-graph-bar/cpu-interrupt . ,(/ (float cpu-irq) cpu-total))
-	      (neato-graph-bar/cpu-vm . ,(/ (float cpu-vm) cpu-total)))))
+	    `((neato-graph-bar-cpu-user . ,(/ (float cpu-user) cpu-total))
+	      (neato-graph-bar-cpu-system . ,(/ (float cpu-system) cpu-total))
+	      (neato-graph-bar-cpu-interrupt . ,(/ (float cpu-irq) cpu-total))
+	      (neato-graph-bar-cpu-vm . ,(/ (float cpu-vm) cpu-total)))))
       ;; First run has cpu-total at 0, which will cause a div-by-0.
       ;; If we find any NaNs, skip drawing
       (unless (cl-find -0.0e+NaN cpu-graph-alist :key #'cdr)
-	(neato-graph-bar/draw-graph cpu-name cpu-graph-alist)))))
+	(neato-graph-bar--draw-graph cpu-name cpu-graph-alist)))))
 
-(defun neato-graph-bar/draw-cpu-graph ()
+(defun neato-graph-bar--draw-cpu-graph ()
   "Draw the CPU graph."
-  (let ((cpu-info (neato-graph-bar/get-cpu-stats)))
-    (if neato-graph-bar/unified-cpu-graph
-	(neato-graph-bar/draw-cpu-graph-helper (car cpu-info))
+  (let ((cpu-info (neato-graph-bar--get-cpu-stats)))
+    (if neato-graph-bar-unified-cpu-graph
+	(neato-graph-bar--draw-cpu-graph-helper (car cpu-info))
       (dolist (cpu (cdr cpu-info))
-	(neato-graph-bar/draw-cpu-graph-helper cpu)
+	(neato-graph-bar--draw-cpu-graph-helper cpu)
 	(insert "\n"))
       (backward-delete-char 1))))
 
-(defun neato-graph-bar/draw-all-graphs ()
+(defun neato-graph-bar--draw-all-graphs ()
   "Draw all available graphs."
-  (neato-graph-bar/draw-cpu-graph)
+  (neato-graph-bar--draw-cpu-graph)
   (insert "\n")
-  (neato-graph-bar/draw-memory-graph)
+  (neato-graph-bar--draw-memory-graph)
   (insert "\n")
-  (neato-graph-bar/draw-swap-graph)
+  (neato-graph-bar--draw-swap-graph)
   (insert "\n"))
 
 (provide 'neato-graph-bar)
